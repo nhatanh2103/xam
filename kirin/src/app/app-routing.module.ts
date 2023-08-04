ac35ee2b4f8a1df0ee0bdc5b867c79845190c7a4
nanh123 <anhnn.bi12-023@st.usth.edu.vn>
2023-08-04 22:20:13 +0700

    add projet

--- kirin/src/app/app-routing.module.ts
+++ kirin/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './Components/shared/header/header.component';
+import { FooterComponent } from './Components/shared/footer/footer.component';
+import { MainLayoutComponent } from './Components/shared/main-layout/main-layout.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        title: ''
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- kirin/src/app/app.module.ts
+++ kirin/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/shared/header/header.component';
+import { FooterComponent } from './Components/shared/footer/footer.component';
+import { MainLayoutComponent } from './Components/shared/main-layout/main-layout.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
